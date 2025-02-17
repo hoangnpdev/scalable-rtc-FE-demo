916724997baa2e680a4bb24720dde783a8ec637b
hoangnp <hoang.np193@gmail.com>
2025-02-17 18:11:38 +0700

    hello stomp

--- src/pages/experiment/experiment.jsx
+++ src/pages/experiment/experiment.jsx
@@ -1,20 +1,33 @@
 import './experiment.css'
+import {useRef, useEffect, useState} from "react";
+import {Client} from '@stomp/stompjs'
 
 function Experiment() {
+    const [msg, setMsg] = useState("");
+    let stompRef = useRef(null);
+    useEffect(() => {
+        stompRef.current = new Client({
+            brokerURL: 'ws://localhost:8080/web-socket',
+            onConnect: () => {
+                stompRef.current.subscribe('/topic/echo', message => {
+                    window.alert(message.body)
+                });
+            },
+        });
+        stompRef.current.activate();
+    }, [])
 
+    const send = () => {
+        console.log(msg)
+        stompRef.current.publish({destination: "/topic/echo", body: msg})
+    }
 
     return (
         <>
-            <div className="container">
-                <div className={'item1'}>Item 1</div>
-                <div>Item 2</div>
-                <div>Item 3</div>
-                <div>Item 4</div>
-                <div>Item 5 (implicit)</div>
-            </div>
-
-
-        </>)
+            <input type={'text'} value={msg} onChange={(e) => setMsg(e.target.value)}/>
+            <button onClick={send}>Send</button>
+        </>
+    )
 }
 
 export default Experiment;
